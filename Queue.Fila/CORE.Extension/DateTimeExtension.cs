e4b811868bd27586826592985296e6db27eaf7e8
Renan VOLKERS <renanvolkers@hotmail.com>
2020-10-30 05:19:46 -0300

    Inclusao classe DateTimeExtension tratamento conversão

---
+++ Queue.Fila/CORE.Extension/DateTimeExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TimeZoneConverter;
+
+namespace CORE.Extension
+{
+    public static class DateTimeExtension
+    {
+        public const string SouthAmericaStandardTime = "E. South America Standard Time";
+        public static readonly TimeZoneInfo SouthAmericaStandardTimeTimeZone = TZConvert.GetTimeZoneInfo(SouthAmericaStandardTime);
+
+        public static DateTime NowBrasiliaTimeZone(this DateTime value) =>
+            TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, SouthAmericaStandardTimeTimeZone);
+
+        public static DateTime ToBrasiliaTimeZone(this DateTime value) =>
+            TimeZoneInfo.ConvertTimeFromUtc(value.ToUniversalTime(), SouthAmericaStandardTimeTimeZone);
+    }
+}
