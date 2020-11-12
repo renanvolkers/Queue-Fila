32de710a1ef72017a405820b00877b94f2dfbece
Renan VOLKERS <renanvolkers@hotmail.com>
2020-11-12 04:25:04 -0300

    Implementando CleamTostartProcess e ProcessQueue

--- Queue.Fila/Queue.Domain/Service/ProcessQueueService.cs
+++ Queue.Fila/Queue.Domain/Service/ProcessQueueService.cs
@@ -1,9 +1,19 @@
 ﻿using Queue.Domain.Service.Interface;
 using System;
+using System.Threading.Tasks;
 
 namespace Queue.Domain
 {
     public class ProcessQueueService : IProcessQueueService
     {
+        public Task CleamToStartProcess()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ProcessQueue()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
